825e1423a6153e001b92c6230f61d23a9843752c
Jack Bittiner <jackbittiner@hotmail.com>
2017-06-12 09:33:46 +0100

    WordCounter splits text into an array

--- spec/wordCounterSpec.js
+++ spec/wordCounterSpec.js
@@ -10,4 +10,12 @@ describe('WordCounter', function() {
       expect(wordCounter.text).toEqual("Hello World! hello world. !hello! .WoRLD.");
     });
   });
+
+  describe('splitWordsToArray', function() {
+    it('splits the text into an array of all the words', function() {
+      wordCounter.splitWordsToArray();
+      expect(wordCounter.arrayOfWords[0]).toEqual('Hello');
+      expect(wordCounter.arrayOfWords.length).toEqual(6);
+    });
+  });
 })
--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,5 +1,9 @@
 'use strict';
 
 function WordCounter(text) {
-  this.text = text
+  this.text = text;
+}
+
+WordCounter.prototype.splitWordsToArray = function() {
+  this.arrayOfWords = this.text.split(" ");
 }
